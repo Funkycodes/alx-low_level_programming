d749c6321c04d4a0074cc385d2ea215417a20c0f
otakus3nnin <otakus3nnin@gmail.com>
2022-04-20 16:52:03 -0700

    done abeg

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,10 +10,14 @@
 void print_array(int *a, int n)
 {
 	int i = 0;
-	
+
+	if (n <= 0)
+	{
+		printf("%d");
+	}
 	while (i < n)
 	{
-		if (i == n-1)
+		if (i == n - 1)
 		{
 			printf("%d\n", a[i]);
 			break;
