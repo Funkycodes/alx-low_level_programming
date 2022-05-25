c10c3c479e9ecedb8cadcea00c5639514fca2c28
otakus3nnin <otakus3nnin@gmail.com>
2022-05-24 19:08:28 -0700

    Ready

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+void print() __attribute__((constructor));
+void print()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+int main(void)
+{
+printf("name");
+}
