b69de9f4613866cca3db3b319b1a3b7d3a066c40
Funkycodes <93059018+Funkycodes@users.noreply.github.com>
2022-05-26 17:00:48 +0100

    Upfate

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,11 @@ void free_listint2(listint_t **head)
 	listint_t *ptr;
 
 	if (*head == NULL)
-		return NULL;
-	while(*head)
+		return;
+	while(*head != NULL)
 	{
 		ptr = *head;
 		*head = ptr->next;
 		free(ptr);
 	}
-
 }
