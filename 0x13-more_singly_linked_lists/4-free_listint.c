9d4c20384a62921a81c1b617acfbb0ca76226ef9
Funkycodes <93059018+Funkycodes@users.noreply.github.com>
2022-05-26 17:01:59 +0100

    Updatw

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,11 +3,13 @@
 void free_listint(listint_t *head)
 {
 	listint_t *ptr;
-	while (head)
+
+        if (head == NULL)
+		return;
+	while (head != NULL)
 	{
 		ptr = head;
 		head = head->next;
 		free(ptr);
 	}
-
 }
