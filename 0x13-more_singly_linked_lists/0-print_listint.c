e7abfa3aa7809ac40956ca7566333c5a04fd1824
otakus3nnin <otakus3nnin@gmail.com>
2022-05-26 06:09:35 -0700

    Rebase

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,18 @@
 #include "lists.h"
-
+/**
+ * _putchar - print character
+ * @c: char to print
+ * Return: 1
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+/**
+ * print_listint - print Elements of a linked list
+ * @h - headnode
+ * Return: length of list
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t i;
