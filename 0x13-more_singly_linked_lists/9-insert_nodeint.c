8921b9a8ff125868595169525586c4d93a1d4404
Funkycodes <93059018+Funkycodes@users.noreply.github.com>
2022-05-27 01:44:21 +0100

    Update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
-
+/**
+ * insert_nodeint_at_index - Inserts a newnode at the specified index
+ * @head: list
+ * @idx: index
+ * @n : data for new node
+ * Return: Adress of new node
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *newnode = malloc(sizeof *newnode);
@@ -12,13 +18,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	node = *head;
 	newnode->n = n;
-	if(ind == 0)
+	if (idx == 0)
 	{
 		newnode->next = node; 
 		*head = newnode;
 		return newnode;
 	}
-	for(j = 0; j < idx - 1;j++)
+	for (j = 0; j < idx - 1;j++)
 	{
 		node=node->next;
 		if (node == NULL)
@@ -29,5 +35,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	newnode->next = node->next;
 	node->next = newnode;
-	return newnode;
+	return (newnode);
 }
