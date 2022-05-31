fe0bcb9c17bec9267b1fd48a5cf8d4fff2f992c5
otakus3nnin <otakus3nnin@gmail.com>
2022-05-30 20:47:33 -0700

    Ahcoolayo

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,12 +9,11 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int bit;
 	unsigned long int total_bits;
 	/*Check if specified index exists for number*/
 	total_bits = (sizeof(total_bits)) * 8;
 	if (index > total_bits)
 		return (-1);
-	bit = (n ! (1 << index));
-	return (bit);
+	*n |= (1 << index);
+	return (1);
 }
